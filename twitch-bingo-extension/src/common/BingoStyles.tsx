28aaa3d5528b10a10d0bf13c6a1a6384233e0497
Christian Corsano <christian.corsano@gmail.com>
2021-07-25 17:08:24 +0200

    Fixed inter-row spacing in grid

--- twitch-bingo-extension/src/common/BingoStyles.tsx
+++ twitch-bingo-extension/src/common/BingoStyles.tsx
@@ -3,7 +3,7 @@ import makeStyles from "@material-ui/core/styles/makeStyles";
 export const bingoStyles = makeStyles({
     paper: {
         background: 'linear-gradient(45deg, rgba(255,255,255,0.8) 0%, rgba(200,200,200,0.8) 100%);',
-        height: '95%',
+        height: '100%',
         borderRadius: '1rem',
         userSelect: 'none',
         textAlign: 'center'
