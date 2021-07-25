795fa819b71e5e52c1cdff06454d8ad0c80d7ed6
Christian Corsano <christian.corsano@gmail.com>
2021-07-26 01:01:48 +0200

    Start de-uglyfying mobile mini-grid

--- twitch-bingo-extension/src/components/Mobile/BingoMobileMiniGrid.tsx
+++ twitch-bingo-extension/src/components/Mobile/BingoMobileMiniGrid.tsx
@@ -21,12 +21,12 @@ export default function BingoMobileMiniGrid(props: BingoMobileMiniGridProps)
         <svg viewBox={`0 0 ${props.columns} ${props.rows / 2}`}>
             <defs>
                 <linearGradient spreadMethod="pad" id="idle" x1="0%" y1="70%" x2="70%" y2="0%">
-                    <stop offset="33%" stopColor="rgb(255, 255, 255)" stopOpacity="0.8" />
-                    <stop offset="90%" stopColor="rgb(200, 200, 200)" stopOpacity="0.8" />
+                    <stop offset="33%" stopColor="rgb(245, 245, 245)" stopOpacity="1" />
+                    <stop offset="90%" stopColor="rgb(255, 255, 255)" stopOpacity="1" />
                 </linearGradient>
                 <linearGradient spreadMethod="pad" id="pending" x1="0%" y1="70%" x2="70%" y2="0%">
-                    <stop offset="33%" stopColor="rgb(122, 196, 255)" stopOpacity="1" />
-                    <stop offset="90%" stopColor="rgb(38, 214, 255)" stopOpacity="0.8" />
+                    <stop offset="33%" stopColor="rgb(240,248,255)" stopOpacity="1" />
+                    <stop offset="90%" stopColor="rgb(240,255,255 )" stopOpacity="1" />
                 </linearGradient>
                 <linearGradient spreadMethod="pad" id="confirmed" x1="0%" y1="70%" x2="70%" y2="0%">
                     <stop offset="33%" stopColor="rgb(223, 255, 50)" stopOpacity="0.8" />
@@ -50,7 +50,7 @@ export default function BingoMobileMiniGrid(props: BingoMobileMiniGridProps)
                         if (! cell)
                         {
                             var key = 1000 + col + (row * this.state.columns);
-                            return <rect key={key} x={col+0.05} y={row+0.025} width="0.9" height="0.45" rx="0.1" ry="0.1" />
+                            return <rect key={key} x={col+0.025} y={row+0.025} width="0.9" height="0.45" rx="0.01" ry="0.01" />
                         }
                         else
                         {
@@ -79,16 +79,16 @@ export default function BingoMobileMiniGrid(props: BingoMobileMiniGridProps)
                             return (
                                 <rect
                                     key={cell.key}
-                                    x={col+0.05}
+                                    x={col+0.025}
                                     y={(row/2.0)+0.025}
-                                    width="0.9"
+                                    width="0.95"
                                     height="0.45"
                                     fill={gradient}
                                     strokeWidth="0.01"
                                     strokeOpacity={isSelected ? "1.0" : "0.5"}
                                     stroke="black"
-                                    rx="0.1"
-                                    ry="0.1"
+                                    rx="0.01"
+                                    ry="0.01"
                                     onClickCapture={(_) => props.onSelectCell(col, row)}
                                 />)
                         }
