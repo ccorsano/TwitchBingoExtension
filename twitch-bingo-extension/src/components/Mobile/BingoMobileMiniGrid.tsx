37b550e5f82c51f5c412e6b19096854624f0f9da
Christian Corsano <christian.corsano@gmail.com>
2021-08-07 17:51:08 +0200

    Mobile view tweaks
    - Switch to symbols for missed & confirmed in minigrid
    - Highlight unactive entries
    - Cleanup minigrid fills

--- twitch-bingo-extension/src/components/Mobile/BingoMobileMiniGrid.tsx
+++ twitch-bingo-extension/src/components/Mobile/BingoMobileMiniGrid.tsx
@@ -24,30 +24,6 @@ export default function BingoMobileMiniGrid(props: BingoMobileMiniGridProps)
     return (
         <svg viewBox={`0 0 ${props.columns} ${props.rows / 2}`}>
             <defs>
-                <linearGradient spreadMethod="pad" id="idle" x1="0%" y1="70%" x2="70%" y2="0%">
-                    <stop offset="33%" stopColor={getRGB(jasminePalette.base)} stopOpacity="1" />
-                    <stop offset="90%" stopColor={getRGB(jasminePalette.base)} stopOpacity="1" />
-                </linearGradient>
-                <linearGradient spreadMethod="pad" id="pending" x1="0%" y1="70%" x2="70%" y2="0%">
-                    <stop offset="33%" stopColor={getRGB(jasminePalette.pending)} stopOpacity="1" />
-                    <stop offset="90%" stopColor={getRGB(jasminePalette.pending)} stopOpacity="1" />
-                </linearGradient>
-                <linearGradient spreadMethod="pad" id="prompt" x1="0%" y1="70%" x2="70%" y2="0%">
-                    <stop offset="33%" stopColor={getRGB(jasminePalette.prompt)} stopOpacity="1" />
-                    <stop offset="90%" stopColor={getRGB(jasminePalette.promptHover)} stopOpacity="1" />
-                </linearGradient>
-                <linearGradient spreadMethod="pad" id="confirmed" x1="0%" y1="70%" x2="70%" y2="0%">
-                    <stop offset="33%" stopColor={getRGB(jasminePalette.confirmed)} stopOpacity="0.8" />
-                    <stop offset="90%" stopColor={getRGB(jasminePalette.pending)} stopOpacity="0.8" />
-                </linearGradient>
-                <linearGradient spreadMethod="pad" id="missed" x1="0%" y1="70%" x2="70%" y2="0%">
-                    <stop offset="33%" stopColor={getRGB(jasminePalette.missed)} stopOpacity="0.8" />
-                    <stop offset="90%" stopColor={getRGB(jasminePalette.missed)} stopOpacity="0.8" />
-                </linearGradient>
-                <linearGradient spreadMethod="pad" id="rejected" x1="0%" y1="70%" x2="70%" y2="0%">
-                    <stop offset="33%" stopColor={getRGB(jasminePalette.missed)} stopOpacity="0.8" />
-                    <stop offset="90%" stopColor={getRGB(jasminePalette.missed)} stopOpacity="0.8" />
-                </linearGradient>
                 <filter id="dropShadow" x="0" y="0" width="110%" height="110%">
                     <feDropShadow dx="0" dy="0" stdDeviation="0.025" floodOpacity="0.3"/>
                 </filter>
@@ -66,41 +42,51 @@ export default function BingoMobileMiniGrid(props: BingoMobileMiniGridProps)
                         else
                         {
                             const isSelected = cell.key === props.selectedKey
-                            var gradient = "url(#idle)"
+                            var fill = isSelected ? getRGB(jasminePalette.prompt) : getRGB(jasminePalette.base)
                             switch (cell.state) {
                                 case BingoEntryState.Pending:
-                                    gradient = "url(#pending)"
+                                    fill = getRGB(jasminePalette.pending)
                                     break;
                                 case BingoEntryState.Confirmed:
-                                    gradient = "url(#confirmed)"
+                                    fill = getRGB(isSelected ? jasminePalette.baseHover : jasminePalette.confirmed)
                                     break;
                                 case BingoEntryState.Missed:
-                                    gradient = "url(#missed)"
+                                    fill = getRGB(isSelected ? jasminePalette.baseHover : jasminePalette.missed)
                                     break;
                                 case BingoEntryState.Rejected:
-                                    gradient = "url(#rejected)"
+                                    fill = getRGB(isSelected ? jasminePalette.baseHover : jasminePalette.missed)
                                     break;
                                 default:
                                     break;
                             }
-                            if (isSelected && cell.state === BingoEntryState.Idle)
-                            {
-                                gradient = "url(#prompt)"
+
+                            var text = ""
+                            switch (cell.state) {
+                                case BingoEntryState.Pending:
+                                    text = LL.Mobile.PendingLabel()
+                                    break;
+                                case BingoEntryState.Confirmed:
+                                    text = "✔"
+                                    break;
+                                case BingoEntryState.Missed:
+                                    case BingoEntryState.Rejected:
+                                    text = "✖"
+                                    break;
+                                default:
+                                    break;
                             }
 
-                            const text = (cell.state === BingoEntryState.Missed || cell.state === BingoEntryState.Rejected) ? LL.Mobile.MissedLabel() : (cell.state === BingoEntryState.Pending) ? LL.Mobile.PendingLabel() : ""
-
                             return (
-                                <g key={cell.key}>
+                                <g key={cell.key} style={{userSelect: 'none'}}>
                                     <rect
                                         x={col+0.025}
                                         y={(row/2.0)+0.025}
                                         width="0.95"
                                         height="0.45"
-                                        fill={gradient}
+                                        fill={fill}
                                         filter="url(#dropShadow)"
                                         strokeWidth="0.01"
-                                        strokeOpacity={isSelected ? "1.0" : "0.5"}
+                                        strokeOpacity={isSelected ? "0.8" : "0.5"}
                                         stroke="black"
                                         rx="0.01"
                                         ry="0.01"
