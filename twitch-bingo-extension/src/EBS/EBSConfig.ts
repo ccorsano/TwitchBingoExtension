bf39f61ba42949de22e50021118e2e921d26af74
Christian Corsano <christian.corsano@gmail.com>
2021-08-15 18:29:37 +0200

    Base AWS Lambda setup
    - In addition to base deployment
      - Manually created a Custom Domain on the API Gateway instance
      - Configured all required parameters and secrets on the Lambda Application as env vars

--- twitch-bingo-extension/src/EBS/EBSConfig.ts
+++ twitch-bingo-extension/src/EBS/EBSConfig.ts
@@ -3,7 +3,7 @@ var server = "https://twitchext.conceptoire.com/bingo/v1"
 var urlParams = new URLSearchParams(window.location.search);
 if (urlParams.get('state') == "testing")
 {
-    server = "https://localhost:5001"
+    server = "https://twitchext-aws-test.conceptoire.com"
 }
 
 
