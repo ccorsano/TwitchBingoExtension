06416d2ae61717472a07cb433083633e714f89fd
Christian Corsano <christian.corsano@gmail.com>
2021-09-11 14:17:48 +0200

    Fix mobile grid sizing

--- twitch-bingo-extension/src/components/Mobile/BingoMobileMiniGrid.tsx
+++ twitch-bingo-extension/src/components/Mobile/BingoMobileMiniGrid.tsx
@@ -21,6 +21,7 @@ export default function BingoMobileMiniGrid(props: BingoMobileMiniGridProps)
     }
 
     const cellSizeWithMargin = 900 / Math.max(props.columns, props.rows)
+    const cellWidthWithMargin = 900 / props.columns
     const cellSize = cellSizeWithMargin-10
     const height = cellSizeWithMargin * props.rows + 180
     const seedStr = React.useMemo(() => `${props.columns}${props.rows}${props.cells.map(c => c.key).join('_')}`, [props.rows, props.columns])
@@ -30,10 +31,10 @@ export default function BingoMobileMiniGrid(props: BingoMobileMiniGridProps)
         <svg className="mobileGrid" viewBox={`0 0 1000 ${height}`}>
             <defs>
             </defs>
-            <rect className="gridBorder" x="10" y="10" width="980" height={height-20} rx="10" ry="10" />
+            <rect className="gridBorder" x="10" y="10" width="980" height={height-20} rx="25" ry="25" />
             <image preserveAspectRatio="xMidYMid meet" x="50" y="0" width={900} height={125} href={headerTitle}/>
             <line className="gridBorder" x1="10" x2={990} y1="127.5" y2="127.5" />
-            <g transform={"translate(0,14)"}>
+            <g transform={"translate(50,155)"}>
                 {
                     [...Array(props.rows).keys()].map(row => {
                         // let isRowComplete = gridContext.isRowComplete(row);
@@ -70,32 +71,32 @@ export default function BingoMobileMiniGrid(props: BingoMobileMiniGridProps)
 
                                 var text = cell.key
 
-                                const baseX = col*cellSizeWithMargin
+                                const baseX = col*cellWidthWithMargin + (cellWidthWithMargin - cellSizeWithMargin)/2
                                 const baseY = row*cellSizeWithMargin
                                 const randX = rand()*(cellSize*0.250)-(cellSize*0.125)
                                 const randY = rand()*(cellSize*0.250)-(cellSize*0.125)
 
                                 const confirmationMark = cell.state == BingoEntryState.Confirmed ? (
-                                    <circle cx={randX+baseX+50+cellSize/2} cy={randY+baseY+135+cellSize/2} r={cellSize/2.25} />
+                                    <circle cx={randX+baseX+cellSizeWithMargin/2} cy={randY+baseY+cellSizeWithMargin/2} r={cellSize/2.25} />
                                 ) : null
 
                                 return (
                                     <g className={classes.join(' ')} key={cell.key} style={{userSelect: 'none'}}>
                                         <rect
                                             className="background"
-                                            x={baseX+50}
-                                            y={baseY+135}
+                                            x={baseX}
+                                            y={baseY}
                                             width={cellSize}
                                             height={cellSize}
                                             rx={cellSize/5}
                                             ry={cellSize/5}
                                             onClickCapture={(_) => props.onSelectCell(col, row)}
                                         />
-                                        <text x={baseX+50+cellSize/2} y={baseY+160+cellSize/2} fontSize={cellSize/2}>{text}</text>
+                                        <text x={baseX+cellSizeWithMargin/2} y={baseY+cellSizeWithMargin/2+cellSize/8} fontSize={cellSize/2}>{text}</text>
                                         { confirmationMark }
                                         <rect
-                                            x={baseX+50}
-                                            y={baseY+135}
+                                            x={baseX}
+                                            y={baseY}
                                             z="10"
                                             width={cellSize}
                                             height={cellSize}
