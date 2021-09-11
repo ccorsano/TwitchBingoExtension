ee1e75ebd427fb29c3db898b2c8116063a19476d
Christian Corsano <christian.corsano@gmail.com>
2021-09-11 11:46:03 +0200

    Redirect to current EBS for review

--- twitch-bingo-extension/src/EBS/EBSConfig.ts
+++ twitch-bingo-extension/src/EBS/EBSConfig.ts
@@ -1,4 +1,4 @@
-var server = "https://bingo-ebs.conceptoire.com"
+var server = "https://twitchext.conceptoire.com/bingo/v1"
 
 var urlParams = new URLSearchParams(window.location.search);
 if (urlParams.get('state') == "testing")
