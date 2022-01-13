e671197177ecc7618035347f82ce8724ac33030e
Christian Corsano <christian.corsano@gmail.com>
2022-01-14 00:31:49 +0100

    Add testing projects

---
+++ BingoBomber/Program.cs
@@ -0,0 +1,88 @@
+﻿using BingoGrain.Model;
+using NBomber;
+using NBomber.Contracts;
+using NBomber.CSharp;
+using NBomber.Plugins.Http.CSharp;
+using System.Net.Http.Json;
+
+var httpFactory = HttpClientFactory.Create();
+
+var bingoGame = new BingoGameCreationParams
+{
+    rows = 3,
+    columns = 3,
+    confirmationThreshold = TimeSpan.FromSeconds(30),
+    enableChatIntegration = false,
+    entries = new BingoEntry[]
+    {
+        new BingoEntry
+        {
+            key = 1,
+            text = "Event 1",
+        },
+        new BingoEntry
+        {
+            key = 2,
+            text = "Event 2",
+        },
+        new BingoEntry
+        {
+            key = 3,
+            text = "Event 3",
+        },
+        new BingoEntry
+        {
+            key = 4,
+            text = "Event 4",
+        },
+        new BingoEntry
+        {
+            key = 5,
+            text = "Event 5",
+        },
+        new BingoEntry
+        {
+            key = 6,
+            text = "Event 6",
+        },
+        new BingoEntry
+        {
+            key = 7,
+            text = "Event 7",
+        },
+        new BingoEntry
+        {
+            key = 8,
+            text = "Event 8",
+        },
+        new BingoEntry
+        {
+            key = 9,
+            text = "Event 9",
+        },
+    }
+};
+
+var step = Step.Create("step", clientFactory: httpFactory, execute: async context => {
+    var request = Http.CreateRequest("POST", "https://localhost:5001/game")
+                        .WithHeader("Accept", "application/json")
+                        .WithBody(JsonContent.Create(bingoGame))
+                        .WithCheck(response =>
+                            Task.FromResult(
+                                response.IsSuccessStatusCode
+                                    ? Response.Ok()
+                                    : Response.Fail()
+                            )
+                        );
+
+    var response = await Http.Send(request, context);
+    return response;
+});
+
+var scenario = ScenarioBuilder
+    .CreateScenario("nbomber-web-site", step)
+    .WithLoadSimulations(Simulation.InjectPerSec(100, TimeSpan.FromSeconds(30)));
+
+NBomberRunner
+    .RegisterScenarios(scenario)
+    .Run();
--- BingoWorker/BingoSiloService.cs
+++ BingoWorker/BingoSiloService.cs
@@ -5,6 +5,7 @@ using Orleans.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -34,9 +35,29 @@ namespace BingoWorker
                 {
                     options.ClusterId = "dev";
                     options.ServiceId = "BingoService";
-                })
-                .UseLocalhostClustering()
-                .ConfigureApplicationParts(app => app.AddApplicationPart(typeof(BingoGameGrain).Assembly).WithReferences());
+                });
+
+            if (_configuration.GetValue<string>("WEBSITE_PRIVATE_IP") != null &&
+                _configuration.GetValue<string>("WEBSITE_PRIVATE_PORTS") != null)
+            {
+                // presume the app is running in Web Apps on App Service and start up
+                IPAddress endpointAddress = IPAddress.Parse(_configuration.GetValue<string>("WEBSITE_PRIVATE_IP"));
+
+                var strPorts = _configuration.GetValue<string>("WEBSITE_PRIVATE_PORTS").Split(',');
+
+                if (strPorts.Length < 2) throw new Exception("Insufficient private ports configured.");
+
+                int siloPort = int.Parse(strPorts[0]);
+                int gatewayPort = int.Parse(strPorts[1]);
+
+                builder.ConfigureEndpoints(endpointAddress, siloPort, gatewayPort);
+            }
+            else
+            {
+                builder.UseLocalhostClustering();
+            }
+            builder.ConfigureApplicationParts(app => app.AddApplicationPart(typeof(BingoGameGrain).Assembly).WithReferences());
+
             return builder.Build();
         }
 
