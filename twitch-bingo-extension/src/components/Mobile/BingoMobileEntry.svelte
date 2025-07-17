114266349a496c1536ea33d0660fce709b884fe5
Christian Corsano <christian.corsano@gmail.com>
2025-07-17 03:35:09 +0200

    Mobile bingo
    - Started porting mobile bingo

---
+++ twitch-bingo-extension/src/components/Mobile/BingoMobileEntry.svelte
@@ -0,0 +1,125 @@
+<script lang="ts">
+    import CountdownCircleTimer from '../../common/CountdownCircleTimer.svelte';
+    import { BingoEntryState, BingoGridCell } from '../../model/BingoEntry';
+    import { FormatTimeout } from '../../EBS/BingoService/EBSBingoTypes';
+    import LL from '../../i18n/i18n-svelte';
+    import { onMount } from 'svelte';
+
+    export let onElementRef: (htmlElement: any) => void
+    export let cell: BingoGridCell
+    export let isSelected: boolean
+    export let onSelect: (key: number) => void
+    export let onConfirm: (key: number) => void
+
+    const cellId = Symbol().toString()
+    let rootElement:any
+
+    onMount(() => {
+        onElementRef(rootElement)
+    })
+
+    let isPrompting = false
+    const isCurrentElementActive = isSelected && cell.state == BingoEntryState.Idle
+
+    $:{
+        if (! isSelected && isPrompting)
+        {
+            isPrompting = false
+        }
+    }
+
+    var bClass = "idle"
+    $:{
+        switch (cell.state) {
+            case BingoEntryState.Pending:
+                bClass = "pending"
+                break;
+            case BingoEntryState.Confirmed:
+                bClass = "confirmed"
+                break;
+            case BingoEntryState.Missed:
+                bClass = "missed"
+                break;
+            case BingoEntryState.Rejected:
+                bClass = "rejected"
+                break;
+            default:
+                break;
+        }
+    }
+    
+    var showTimer: boolean = false
+    $: showTimer = cell.timer != null
+    var duration: number = 0
+    $: showTimer ? (cell.timer!.getTime() - Date.now()) / 1000 : 0
+    var classes: string[] = ["bingoEntry", bClass]
+    $:{
+        if (isSelected)
+        {
+            if (isPrompting)
+            {
+                classes.push("prompting")
+            }
+            else
+            {
+                classes.push("highlighted")
+            }
+            classes.push("prompt")
+        }
+    }
+    let remainingTime: number = duration
+
+    const confirmKey = (key: number) => {
+        isPrompting = false
+        onConfirm(key)
+    }
+</script>
+
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div id={cellId} bind:this={rootElement}
+        class={classes.join(" ")}
+        on:click|capture={(_) => {
+            if (isCurrentElementActive)
+            {
+                isPrompting = !isPrompting
+            } else {
+                onSelect(cell.key)
+            }
+        }}
+        style:position="relative">
+    <div class='key'>{cell.key}</div>
+    <div class='text'>
+        {cell.text}
+    </div>
+    <div
+        class={"bingoCellPrompt"}
+        class:bingoCellPromptVisible={isCurrentElementActive && isPrompting}
+        class:bingoCellPromptHidden={!(isCurrentElementActive && isPrompting)}
+        on:click|capture={ isPrompting ? (_) => confirmKey(cell.key) : null} >
+        {$LL.Mobile.ConfirmButton()}
+    </div>
+    <div class={"bingoCellTimer"}>
+         class:bingoCellTimerVisible={showTimer && duration > 0}
+         class:bingoCellTimerHidden={!(showTimer && duration > 0)}
+        <div style:display="inline-block">
+            {#if showTimer && duration > 0}
+                <CountdownCircleTimer
+                    isPlaying
+                    size={50}
+                    strokeWidth={3}
+                    colors="#000"
+                    duration={duration}
+                    onUpdate={elapsedTime => {
+                        remainingTime = duration - elapsedTime
+                    }}
+                >
+                    <span style:font-size="14px">
+                        {FormatTimeout(remainingTime)}
+                    </span>
+                </CountdownCircleTimer>
+            {/if}
+        </div>
+    </div>
+</div>
---
+++ twitch-bingo-extension/src/components/Mobile/BingoMobileMiniGrid.svelte
@@ -0,0 +1,122 @@
+<script lang="ts">
+    import { seededRand } from "../../common/ExtensionUtils";
+    import { BingoEntryState, BingoGridCell } from "../../model/BingoEntry";
+    const headerTitle = require("../../../assets/BingoHeaderMobile.svg")
+
+    export let cells: Array<BingoGridCell> = new Array(0)
+    export let rows: number
+    export let columns: number
+    export let canVote: boolean
+    export let onSelectCell: (x: number, y: number) => void
+    export let selectedKey: number | null = null
+
+    function getCell(row: number, col: number)
+    {
+        return cells.find(c => c.row == row && c.col == col)
+    }
+
+    const cellSizeWithMargin = 900 / Math.max(columns, rows)
+    const cellWidthWithMargin = 900 / columns
+    const cellSize = cellSizeWithMargin-10
+    const height = cellSizeWithMargin * rows + 180
+    let seedStr: string = ""
+    $: seedStr = `${columns}${rows}${cells.map(c => c.key).join('_')}`, [rows, columns]
+    const rand = seededRand(seedStr)
+
+    function getEntryInfos(row: number, col: number) {
+        let cell = getCell(row, col)
+        if (!cell) {
+            return null
+        }
+
+        const isSelected = cell.key === selectedKey
+
+        var classes: string[] = Array();
+
+        switch (cell.state) {
+            case BingoEntryState.Confirmed:
+                classes.push("confirmed")
+                break;
+            case BingoEntryState.Idle:
+                classes.push("idle")
+                break;
+            case BingoEntryState.Missed:
+            case BingoEntryState.Rejected:
+                classes.push("missed")
+                break;
+            case BingoEntryState.Pending:
+                classes.push("pending")
+                break;
+            default:
+                break;
+        }
+        if (isSelected)
+        {
+            classes.push("highlighted")
+        }
+
+        return {
+            row: row,
+            col: col,
+            text: cell.key,
+            cell: cell,
+            classes: classes,
+            baseX: col*cellWidthWithMargin + (cellWidthWithMargin - cellSizeWithMargin)/2,
+            baseY: row*cellSizeWithMargin,
+            randX: rand()*(cellSize*0.250)-(cellSize*0.125),
+            randY: rand()*(cellSize*0.250)-(cellSize*0.125),
+        }
+    }
+
+    const entriesInfos = [...Array(rows).keys()].map(row => [...Array(columns).keys()].map(col => getEntryInfos(row, col))).flat(1)
+</script>
+
+<style lang="scss">
+    @use "./BingoMobileMiniGrid.scss"
+</style>
+
+<svg class="mobileGrid" viewBox={`0 0 1000 ${height}`}>
+    <defs>
+    </defs>
+    <rect class="gridBorder" x="10" y="10" width="980" height={height-20} rx="25" ry="25" />
+    <image preserveAspectRatio="xMidYMid meet" x="50" y="0" width={900} height={125} href={headerTitle}/>
+    <line class="gridBorder" x1="10" x2={990} y1="127.5" y2="127.5" />
+    <g transform="translate(50,155)">
+        {#each entriesInfos as entryInfo}
+            {#if entryInfo}
+                <!-- svelte-ignore a11y-click-events-have-key-events -->
+                <g class={entryInfo.classes.join(' ')} style="user-select: 'none';">
+                    <!-- svelte-ignore a11y-click-events-have-key-events -->
+                    <!-- svelte-ignore a11y-no-static-element-interactions -->
+                    <rect
+                        class="background"
+                        x={entryInfo.baseX}
+                        y={entryInfo.baseY}
+                        width={cellSize}
+                        height={cellSize}
+                        rx={cellSize/5}
+                        ry={cellSize/5}
+                        on:click|capture={(_) => onSelectCell(entryInfo.col, entryInfo.row)}
+                    />
+                    <text x={entryInfo.baseX+cellSizeWithMargin/2} y={entryInfo.baseY+cellSizeWithMargin/2+cellSize/8} font-size={cellSize/2}>{entryInfo.text}</text>
+                    {#if entryInfo.cell.state == BingoEntryState.Confirmed}
+                        <circle cx={entryInfo.randX+entryInfo.baseX+cellSizeWithMargin/2}
+                                cy={entryInfo.randY+entryInfo.baseY+cellSizeWithMargin/2}
+                                r={cellSize/2.25} />
+                    {/if}
+                    <!-- svelte-ignore a11y-click-events-have-key-events -->
+                    <!-- svelte-ignore a11y-no-static-element-interactions -->
+                    <rect
+                        x={entryInfo.baseX}
+                        y={entryInfo.baseY}
+                        z="10"
+                        width={cellSize}
+                        height={cellSize}
+                        fill="rgba(255,255,255,0.0)"
+                        on:click|capture={(_) => onSelectCell(entryInfo.col, entryInfo.row)}
+                    />
+                </g>
+            {/if}
+        {/each}
+    </g>
+</svg>
